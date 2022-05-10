db12fd1263d92c3f66937ea8b80d52e67a675b98
aditnag <iamaditnag034@gmail.com>
2022-05-10 13:33:36 +0530

    mod

--- value_error.py
+++ value_error.py
@@ -11,4 +11,27 @@ def value_error():
         print(f"x is {x}")
 
 
-value_error()
+# This function will go on until user inputs a valid integer
+def value_error_new_solution():
+    while True:
+        try:
+            x = int(input("What's the value for x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    print(f"x is {x}")
+
+
+def main():
+    x = int(input("Enter your choice 1 or 2: "))
+    if x == 1:
+        value_error()
+    elif x == 2:
+        value_error_new_solution()
+    else:
+        print("Invalid choice!")
+
+
+if __name__ == "__main__":
+    main()
