0565d2a427b80f362c424963e90bfd906298a28e
aditnag <iamaditnag034@gmail.com>
2022-05-10 13:20:30 +0530

    comment

--- value_error.py
+++ value_error.py
@@ -5,6 +5,8 @@ def value_error():
     except ValueError:
         print("x is not an integer!")
 
+    # print(f"x is {x}") will cause ValueError in line 3
+    # as int fn will throw error for string value and x will not be updated
     else:
         print(f"x is {x}")
 
