df3817bf3405e45c2655c043a9d4eec8092bd1e5
aditnag <iamaditnag034@gmail.com>
2022-07-14 23:38:09 +0530

    correction using split()

--- find_point.py
+++ find_point.py
@@ -1,29 +1,14 @@
-class Point:
-    def __init__(self):
-        self.px = None
-        self.py = None
-        self.qx = None
-        self.qy = None
-
+class PointsOperation:
     def find_point(self, px, py, qx, qy):
-        self.px = px
-        self.py = py
-        self.qx = qx
-        self.qy = qy
-        rx = 2*self.qx - self.px
-        ry = 2*self.qy - self.py
-        return rx, ry
+        print(2 * qx - px, 2 * qy - py)
 
 
-try:
-    px = int(float(input()))
-    py = int(float(input()))
-    qx = int(float(input()))
-    qy = int(float(input()))
-    if -100 <= px <= 100 and -100 <= py <= 100 and -100 <= qx <= 100 and -100 <= qy <= 100:
-        point = Point()
-        print(point.find_point(px, py, qx, qy))
-    else:
-        print("Numbers out of range")
-except:
-    print("Invalid input")
+inp = input()
+i = inp.split()
+# print(i)
+px = int(i[0])
+py = int(i[1])
+qx = int(i[2])
+qy = int(i[3])
+point = PointsOperation()
+point.find_point(px, py, qx, qy)
